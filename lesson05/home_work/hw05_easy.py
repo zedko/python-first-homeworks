2e340e72c28ff3b29ff64d3950dd9cfaf048336a
zedko <zedko@bk.ru>
2019-01-28 15:54:08 +0300

    Переработан файл в библиотеку для импорта в задание NORMAL

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -10,29 +10,40 @@ import sys
 curr_dir = os.getcwd()
 
 
-def create_dir(dir_amount):
-    dir_names = ["dir"+str(x) for x in range(dir_amount)]  # generate names
+def create_dir(dir_prefix: str = "dir", dir_amount: int = 1):
+    if dir_amount == 1:
+        dir_names = [dir_prefix]  # a single directory
+    else:
+        dir_names = [dir_prefix+str(x) for x in range(int(dir_amount))]  # generate names for many directories
     for i in dir_names:  # creating dirs
         os.mkdir(os.path.join(curr_dir, i))
 
 
-def delete_dir(dir_amount):
-    dir_names = ["dir" + str(x) for x in range(dir_amount)]  # generate names
+def delete_dir(dir_prefix: str = "dir", dir_amount: int = 1):
+    if dir_amount == 1:
+        dir_names = [dir_prefix]  # a single directory
+    else:
+        dir_names = [dir_prefix+str(x) for x in range(int(dir_amount))]  # generate names for many directories
     for i in dir_names:  # deleting dirs
         os.rmdir(os.path.join(curr_dir, i))
 
 
-create_dir(10)
-delete_dir(10)
+
+
+# create_dir(10)
+# delete_dir(10)
 # print(curr_dir, dir_names)
 
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
-list_all = os.listdir(curr_dir)  # get files and dir in current dir
-for x in list_all:
-    if os.path.isdir(x):
-        print(x)
+def show_folders():
+    dir_index = 0
+    list_all = os.listdir(curr_dir)  # get files and dir in current dir
+    for x in list_all:
+        if os.path.isdir(x):
+            dir_index += 1
+            print(dir_index, "...", x)
 
 
 # Задача-3:
