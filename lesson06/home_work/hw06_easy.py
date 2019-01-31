e3d7f26d5569b82482e5c700e630858ced9d55ee
zedko <zedko@bk.ru>
2019-01-31 15:40:46 +0300

    Решено easy 1 В процессе решение normal

--- lesson06/home_work/hw06_easy.py
+++ lesson06/home_work/hw06_easy.py
@@ -1,9 +1,50 @@
+import math
 # Задача-1: Написать класс для фигуры-треугольника, заданного координатами трех точек.
 # Определить методы, позволяющие вычислить: площадь, высоту и периметр фигуры.
+
+class Triangle(object):
+    x1: int
+    y1: int
+    x2: int
+    y2: int
+    x3: int
+    y3: int
+    square: float
+    side_length: float
+    height: float
+
+    def __init__(self, x1, y1, x2, y2, x3, y3):
+        self.x1 = int(x1)
+        self.y1 = int(y1)
+        self.x2 = int(x2)
+        self.y2 = int(y2)
+        self.x3 = int(x3)
+        self.y3 = int(y3)
+
+    def get_tri_square(self):
+        self.square = (1 / 2) * abs(((self.x2 - self.x1) * (self.y3 - self.y1))
+                                             - ((self.x3 - self.x1) * (self.y2 - self.y1)))
+
+    def get_tri_side_len(self):
+        self.side_length = math.sqrt(((self.x2 - self.x1)**2) + ((self.y2 - self.y1)**2)) \
+                                    + math.sqrt(((self.x3 - self.x2)**2) + ((self.y3 - self.y2)**2)) \
+                                    + math.sqrt(((self.x3 - self.x1)**2) + ((self.y3 - self.y1)**2))
+
+    def get_tri_height(self):
+        self.get_tri_square()
+        self.height = (2 * self.square) - math.sqrt(((self.x2 - self.x1)**2) + ((self.y2 - self.y1)**2))
+
+
+triangle1 = Triangle(-1, -3, 3, 4, 5, -5)
+
+triangle1.get_tri_square()
+triangle1.get_tri_height()
+triangle1.get_tri_side_len()
+print(triangle1.square, triangle1.side_length, triangle1.height)
+
 
 
 # Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
 # Предусмотреть в классе методы:
 # проверка, является ли фигура равнобочной трапецией;
 # вычисления: длины сторон, периметр, площадь.
-
--- lesson06/home_work/hw06_normal.py
+++ lesson06/home_work/hw06_normal.py
@@ -7,6 +7,75 @@
 # Т.е. Учитель Иванов может преподавать математику у 5А и 6Б,
 # но больше математику не может преподавать никто другой.
 
+
+class Teacher(object):
+    t_name: str  # FIO
+    subject: str  # chemistry
+
+    def __init__(self, name, subj):
+        self.t_name = name
+        self.subject = subj
+
+
+class Student(object):
+    s_name: str  # FIO
+    group: str  # 7A
+    father_name: str  # Alex
+    mother_name: str  # Olga
+    
+
+    def __init__(self, name, group, father, mother):
+        self.s_name = name
+        self.group = group
+        self.father_name = father
+        self.mother_name = mother
+
+    def get_student_group(self):
+        return self.group
+
+# class Learning(Teacher, Student):
+
+
+stud_1_test = Student("Sergey Bodrov", "7A", "Alexey", "Yulya")
+stud_2_test = Student("Jenya Ivanova", "7A", "Ivan", "Olesya")
+stud_3_test = Student("Daniil Rostkov", "6B", "Nikolai", "Irina")
+student_list = [stud_1_test, stud_2_test, stud_3_test]
+
+
+def add_student():  # дополнение списка для тестов
+    name = input(str("Name: "))
+    group = input(str("Group: "))
+    father = input(str("Father: "))
+    mother = input(str("Mother: "))
+    student_list.append(Student(name, group, father, mother))
+
+
+while True:
+    question = input(str("Do you want to add a student? Y/N:  "))
+    if question == "Y":
+        add_student()
+    else:
+        break
+
+print(student_list)
+
+
+def get_groups():
+    groups = set([x.get_student_group() for x in student_list])
+    return groups
+
+
+def get_students_in_group(group):
+
+    pass
+
+def get_student_lessons(student_name):
+    pass
+def get_student_parents(student_name):
+    pass
+def get_group_teachers(group):
+    pass
+
 # Выбранная и заполненная данными структура должна решать следующие задачи:
 # 1. Получить полный список всех классов школы
 # 2. Получить список всех учеников в указанном классе
@@ -15,3 +84,6 @@
 #  (Ученик --> Класс --> Учителя --> Предметы)
 # 4. Узнать ФИО родителей указанного ученика
 # 5. Получить список всех Учителей, преподающих в указанном классе
+
+
+print("All groups:", get_groups())
